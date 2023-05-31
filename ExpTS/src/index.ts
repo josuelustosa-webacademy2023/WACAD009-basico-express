42e8742b440c6dc01b6b6815d9def51a0905fd1a
Josué Lustosa <josuelustosa.job@gmail.com>
2023-05-30 20:38:39 -0400

    feat: funcao middlewares para salvar logs em arquivo txt e lendo arquivos estaticos

--- ExpTS/src/index.ts
+++ ExpTS/src/index.ts
@@ -3,14 +3,17 @@ import validateEnv from './utils/validateEnv';
 import dotenv from 'dotenv';
 //import morgan from 'morgan';
 import logger from './middlewares/logger';
+import loggerUser from './middlewares/loggerUser';
 
 dotenv.config();
 validateEnv();
 
 const app = express();
 const PORT = process.env.PORT ?? 3333;
+const publicPath = `${process.cwd()}/public`;
 
-app.use(logger('completo'));
+app.use(logger('completo')); // Gerando logs no console
+app.use(loggerUser('completo')); // Salvando logs em "logs/logs.txt"
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log('Hello World!');
@@ -20,6 +23,15 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.get('/', (req, res) => {
   res.send('Aplicação Express utilizando TypeScript!');
 });
+
+// Lendo um arquivo (HMTL)
+app.get('/statics-files-page', (req, res) => {
+  res.sendFile(`${publicPath}/html/index.html`);
+});
+
+// Renderizando arquivos estáticos
+app.use('/css/style.css', express.static(`${publicPath}/css/style.css`));
+app.use('/js/main.js', express.static(`${publicPath}/js/main.js`));
 
 app.listen(PORT, () => {
   console.log(`App Express iniciada na porta ${PORT}.`);
