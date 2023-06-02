94b2d3112f386ed040e0fb0cccf7cdd9279f6ca3
Josué Lustosa <josuelustosa.job@gmail.com>
2023-06-01 20:40:55 -0400

    feat: movendo o express para MVC

---
+++ ExpTS/src/controllers/main.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express-serve-static-core';
+
+const publicPath = `${process.cwd()}/public`;
+
+const home = (req: Request, res: Response) => {
+  res.send(
+    `
+        <h1>Bem-vindo :)</h1>
+        <h2>Aplicação Express utilizando TypeScript</h2>    
+    `,
+  );
+};
+
+const welcome = (req: Request, res: Response) => {
+  res.send(req.params.nome);
+};
+
+const staticsFiles = (req: Request, res: Response) => {
+  res.sendFile(`${publicPath}/html/index.html`);
+};
+
+const hb1 = (req: Request, res: Response) => {
+  res.render('hb1', {
+    uf: 'Universidade Federal do Amazonas',
+    layout: false,
+  });
+};
+
+const hb2 = (req: Request, res: Response) => {
+  const profs = [
+    { nome: 'Tayana Conte', sala: 1234 },
+    { nome: 'Horácio Fernandes', sala: 1235 },
+    { nome: 'Edleno Moura', sala: 1236 },
+    { nome: 'Elaine Harada', sala: 1237 },
+  ];
+
+  res.render('hb2', {
+    profs,
+    layout: false,
+  });
+};
+
+const hb3 = (req: Request, res: Response) => {
+  res.render('hb3', {
+    nome: 'Express',
+    tipo: 'Framework',
+    poweredByNode: true,
+    layout: false,
+  });
+};
+
+const hb4 = (req: Request, res: Response) => {
+  const profs = [
+    { nome: 'Tayana Conte', sala: 1234 },
+    { nome: 'Horácio Fernandes', sala: 1235 },
+    { nome: 'Edleno Moura', sala: 1236 },
+    { nome: 'Elaine Harada', sala: 1237 },
+  ];
+
+  res.render('hb4', {
+    profs,
+    layout: false,
+  });
+};
+
+export default { welcome, home, staticsFiles, hb1, hb2, hb3, hb4 };
--- ExpTS/src/router/router.ts
+++ ExpTS/src/router/router.ts
@@ -1,58 +1,20 @@
 import { Router } from 'express';
+import mainController from '../controllers/main';
 
 const router = Router();
-const publicPath = `${process.cwd()}/public`;
 
-router.get('/', (req, res) => {
-  res.send('Aplicação Express utilizando TypeScript!');
-});
+router.get('/', mainController.home);
 
-router.get('/statics-files-page', (req, res) => {
-  res.sendFile(`${publicPath}/html/index.html`);
-});
+router.get('/welcome/:nome', mainController.welcome);
 
-router.get('/hb1', (req, res) => {
-  res.render('hb1', {
-    uf: 'Universidade Federal do Amazonas',
-    layout: false,
-  });
-});
+router.get('/statics-files-page', mainController.staticsFiles);
 
-router.get('/hb2', (req, res) => {
-  const profs = [
-    { nome: 'Tayana Conte', sala: 1234 },
-    { nome: 'Horácio Fernandes', sala: 1235 },
-    { nome: 'Edleno Moura', sala: 1236 },
-    { nome: 'Elaine Harada', sala: 1237 },
-  ];
+router.get('/hb1', mainController.hb1);
 
-  res.render('hb2', {
-    profs,
-    layout: false,
-  });
-});
+router.get('/hb2', mainController.hb2);
 
-router.get('/hb3', (req, res) => {
-  res.render('hb3', {
-    nome: 'Express',
-    tipo: 'Framework',
-    poweredByNode: true,
-    layout: false,
-  });
-});
+router.get('/hb3', mainController.hb3);
 
-router.get('/hb4', (req, res) => {
-  const profs = [
-    { nome: 'Tayana Conte', sala: 1234 },
-    { nome: 'Horácio Fernandes', sala: 1235 },
-    { nome: 'Edleno Moura', sala: 1236 },
-    { nome: 'Elaine Harada', sala: 1237 },
-  ];
-
-  res.render('hb4', {
-    profs,
-    layout: false,
-  });
-});
+router.get('/hb4', mainController.hb4);
 
 export default router;
