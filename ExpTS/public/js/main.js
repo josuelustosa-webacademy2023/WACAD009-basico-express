9caa2173bfef798b0ac6b3e244bfab5e6be22a4b
Josué Lustosa <josuelustosa.job@gmail.com>
2023-06-05 16:53:43 -0400

    fix: alterando estilo de h1 e nome de usuario

--- ExpTS/public/js/main.js
+++ ExpTS/public/js/main.js
@@ -2,6 +2,6 @@ const userName = prompt('Informe seu nome');
 
 const element = document.querySelector('.title');
 
-let message = document.createTextNode(`Welcome ${userName}!`);
+let message = document.createTextNode(`Bem-vindo(a) ${userName ? userName : 'Zé Ninguém'}!`);
 
 element.appendChild(message);
